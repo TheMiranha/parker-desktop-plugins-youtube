3bfb0a254a80d0dbb93ce519b2c8f2f21e3c330a
TheMiranha <lxbrmiranda@gmail.com>
2022-05-30 01:55:31 -0300

    first commit

---
+++ home/sidebar.js
@@ -0,0 +1,10 @@
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+const Item = () => {
+    return {
+        title: "Youtube",
+        icon: <YouTubeIcon style={{marginRight: 10}}/>
+    }
+}
+
+export default { Item }
